9062685ff7188c21f6212ccc1b7c4da07a9942f1
Joshua <xiaozh620@gmail.com>
2020-06-02 23:54:38 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/xzh/springcloud/config/GatewayConfig.java
@@ -0,0 +1,22 @@
+package com.xzh.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("xroute",
+                r -> r.path("/baidu")
+                        .uri("https://www.baidu.com")).build();
+
+        return routes.build();
+    }
+}
